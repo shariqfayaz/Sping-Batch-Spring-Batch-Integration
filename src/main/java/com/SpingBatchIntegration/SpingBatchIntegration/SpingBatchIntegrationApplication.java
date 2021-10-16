3e0a9e1d06aa1a7d90b62379efbb5a7dae1f316d
shariqfayaz <79368592+shariqfayaz@users.noreply.github.com>
2021-10-16 14:45:17 +0530

    project steup

---
+++ src/main/java/com/SpingBatchIntegration/SpingBatchIntegration/SpingBatchIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.SpingBatchIntegration.SpingBatchIntegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBatchIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBatchIntegrationApplication.class, args);
+	}
+
+}
